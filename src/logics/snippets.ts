3969038a5da14ee06140b753d23c8bdd32f22f81
Felix <KLDXYO@outlook.com>
2026-02-27 22:39:25 +0800

    feat: replaced the original dollar sign with the XOR symbol

--- src/logics/snippets.ts
+++ src/logics/snippets.ts
@@ -5,7 +5,7 @@ export function buildCompletionItem(config: SnippetConfigItem, language: string)
     if (![language, '*'].includes(config.main['@_language'])) {
         return;
     }
-    const snippet = new vscode.CompletionItem(config.key, vscode.CompletionItemKind.Snippet);
+
     const rawText = config.main['#text'].trimEnd().split('\n');
     while (rawText.length > 0 && !rawText[0]) {
         rawText.shift();
@@ -13,15 +13,17 @@ export function buildCompletionItem(config: SnippetConfigItem, language: string)
     if (rawText.length === 0) {
         return;
     }
-    const firstLine = rawText[0];
-    const leadSpace = firstLine.length - firstLine.trimStart().length;
-    const text = rawText
+    const leadSpace = rawText[0].length - rawText[0].trimStart().length;
+    var text = rawText
         .map(line => {
             const a = line.trimStart();
             const b = line.slice(leadSpace);
             return a.length >= b.length ? a : b;
         })
         .join('\n');
+
+    const snippet = new vscode.CompletionItem(config.key, vscode.CompletionItemKind.Snippet);
+    text = text.replace(/\$/g, "\\$").replace(/\^/g, "$");
     snippet.insertText = new vscode.SnippetString(text);
     snippet.detail = config.tip;
     return snippet;
